e1f3c0045f8193eb4167c39b2c172e0f3ddbc850
klein <zhoukaihf@foxmail.com>
2017-07-17 09:01:20 +0800

    Apply new implementation for jni interface for platinum
    Signed-off-by: klein <zhoukaihf@foxmail.com>

--- app/src/main/java/com/quectel/platinumdlna/PlatinumDLNA.java
+++ app/src/main/java/com/quectel/platinumdlna/PlatinumDLNA.java
@@ -3,27 +3,23 @@ package com.quectel.platinumdlna;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import com.plutinosoft.platinum.UPnP;
 
 public class PlatinumDLNA extends AppCompatActivity {
+
+    UPnP uPnP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_platinum_dlna);
 
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+        uPnP = new UPnP();
+        uPnP.start();
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(uPnP.getVersion());
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
 }
