e2680952cb699a271de8c4efa371e0a5948ed164
klein <zhoukaihf@foxmail.com>
2017-07-21 14:15:11 +0800

    Add version tag 1.0.2
    Signed-off-by: klein <zhoukaihf@foxmail.com>

--- app/src/main/java/com/quectel/platinumdlna/PlatinumDLNA.java
+++ app/src/main/java/com/quectel/platinumdlna/PlatinumDLNA.java
@@ -2,12 +2,19 @@ package com.quectel.platinumdlna;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import com.plutinosoft.platinum.UPnP;
 
 public class PlatinumDLNA extends AppCompatActivity {
 
+    public static final String TAG = "PlatinumDLNA";
+
     UPnP uPnP;
+    Button btShowDMS, btShowDMR;
+    String[] version = new String[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +22,32 @@ public class PlatinumDLNA extends AppCompatActivity {
         setContentView(R.layout.activity_platinum_dlna);
 
         uPnP = new UPnP();
-        uPnP.start();
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(uPnP.getVersion());
+        //checkVersion must be called before we start upnp.
+        if (uPnP.checkVersion(version)){
+            Log.d(TAG,"version check ok " + version[0]);
+        } else {
+            Log.d(TAG,"expected version of platinum sdk is " + version[1] + ", but we got " + version[0]);
+        }
+
+
+        Log.d(TAG, "uPnP.start() = " + uPnP.start());
+
+        btShowDMS = (Button) findViewById(R.id.show_dms);
+        btShowDMR = (Button) findViewById(R.id.show_dmr);
+
+        btShowDMS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "uPnP.start() = " + uPnP.getms());
+            }
+        });
+
+
+
+        // Examp le of a call to a native method
+       // TextView tv = (TextView) findViewById(R.id.sample_text);
+       // tv.setText(uPnP.getVersion());
     }
 
 }
--- platinumcore/src/main/java/com/plutinosoft/platinum/UPnP.java
+++ platinumcore/src/main/java/com/plutinosoft/platinum/UPnP.java
@@ -1,6 +1,14 @@
 package com.plutinosoft.platinum;
 
+import android.util.Log;
+
+
 public class UPnP {
+    public static final String TAG = "PlatinumDLNA";
+    public static final String VERSION_OF_PLATITUMDLNA = "1.0.2";
+
+
+
     public UPnP() {
         cSelf = _init();
     }
@@ -13,15 +21,35 @@ public class UPnP {
         return _stop(cSelf);
     }
 
-    public String getVersion() {
-        return _getversion();
+    public String setms() {
+        return _setms();
+    }
+
+    public String getms() {
+        return _getms();
+    }
+
+    public boolean checkVersion(String[] version) {
+        String version_of_sdk = _checkVersion();
+        version[0] = version_of_sdk;
+        version[1] = VERSION_OF_PLATITUMDLNA;
+        if (version[0].equals(VERSION_OF_PLATITUMDLNA)){
+            return true;
+        }
+        return false;
+    }
+
+    public void onDmsAdded(String device){
+        Log.d(TAG, "message from JNI native,device = " + device);
     }
 
     // C glue
-    private static native long _init();
-    private static native int _start(long self);
-    private static native int _stop(long self);
-	private static native String _getversion();
+    private native long _init();
+    private native int _start(long self);
+    private native int _stop(long self);
+    private native String _setms();
+    private native String _getms();
+	private native String _checkVersion();
 	
     private final long cSelf;
 
